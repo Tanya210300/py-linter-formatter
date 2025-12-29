1d6db35ca053e0f08beeeb51172df4c2933282ba
Tanya <tanya210300@gmail.com>
2025-12-29 21:55:34 +0200

    correct2

--- app/main.py
+++ app/main.py
@@ -7,16 +7,12 @@ def format_linter_error(error: dict) -> dict:
         "source": "flake8",
     }
 
-
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "path": file_path,
         "status": "failed" if errors else "passed",
         "errors": [format_linter_error(error) for error in errors],
     }
-
-   
-
 
 def format_linter_report(linter_report: dict) -> list:
     return [
